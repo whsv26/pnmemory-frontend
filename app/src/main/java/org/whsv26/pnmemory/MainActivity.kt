a3c523eb1f6716fb2d5c9d7c4f6cd15d8bb42af9
whsv26 <whsv26@gmail.com>
2021-08-03 22:37:58 +0300

    Refresh fcm token button. Simple toast output.

--- app/src/main/java/org/whsv26/pnmemory/MainActivity.kt
+++ app/src/main/java/org/whsv26/pnmemory/MainActivity.kt
@@ -2,10 +2,21 @@ package org.whsv26.pnmemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.installations.FirebaseInstallations
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val refreshRcmTokenButton: Button = findViewById(R.id.button_refresh_rcm_token)
+    refreshRcmTokenButton.setOnClickListener {
+      FirebaseInstallations.getInstance().getToken(false).addOnSuccessListener { res ->
+        Toast.makeText(this, res.token, Toast.LENGTH_LONG).show()
+      }
+    }
   }
 }
