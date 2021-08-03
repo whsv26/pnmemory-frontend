60ca9c92709e7e716d7b4afe5cd29e98aaaca30f
whsv26 <whsv26@gmail.com>
2021-08-03 21:58:47 +0300

    First commit

---
+++ app/src/main/java/org/whsv26/pnmemory/PnmemoryFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package org.whsv26.pnmemory
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+object PnmemoryFirebaseMessagingService : FirebaseMessagingService() {
+  override fun onNewToken(token: String) {
+
+  }
+
+  override fun onMessageReceived(msg: RemoteMessage) {
+
+  }
+}
