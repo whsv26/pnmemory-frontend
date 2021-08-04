484b091a9e9a6cc7a77da60b65568a41f904fe49
whsv26 <whsv26@gmail.com>
2021-08-04 22:47:43 +0300

    Login request.

--- app/src/main/java/org/whsv26/pnmemory/api/Backend.kt
+++ app/src/main/java/org/whsv26/pnmemory/api/Backend.kt
@@ -3,8 +3,11 @@ package org.whsv26.pnmemory.api
 import android.content.SharedPreferences
 import arrow.core.Option
 import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.fuel.core.deserializers.EmptyDeserializer
 import com.github.kittinunf.fuel.gson.jsonBody
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.fuel.httpPut
+import org.whsv26.pnmemory.api.request.LogInRequest
 import org.whsv26.pnmemory.api.request.RefreshFcmTokenRequest
 
 object Backend {
@@ -15,7 +18,7 @@ object Backend {
     FuelManager.instance.baseHeaders = headers
   }
 
-  fun authorize(preference: SharedPreferences) {
+  private fun authorize(preference: SharedPreferences) {
     Option
       .fromNullable(preference.getString("jwt_token", null))
       .map { headers.put("Authorization", "Bearer $it") };
@@ -23,6 +26,10 @@ object Backend {
 
   operator fun invoke(preference: SharedPreferences): Backend {
     return apply { authorize(preference) }
+  }
+
+  suspend fun login(request: LogInRequest): ResponseOf<Unit> {
+    return "public/token".httpPost().jsonBody(request).awaitResponse(EmptyDeserializer)
   }
 
   suspend fun refreshFcmToken(request: RefreshFcmTokenRequest) {
