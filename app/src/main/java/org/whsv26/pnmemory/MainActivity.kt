ef57e3dbc64a2f7229f667dbdfdc8b691477c7ba
whsv26 <whsv26@gmail.com>
2021-08-07 23:02:14 +0300

    Replace find view with view binding for main activity.

--- app/src/main/java/org/whsv26/pnmemory/MainActivity.kt
+++ app/src/main/java/org/whsv26/pnmemory/MainActivity.kt
@@ -11,30 +11,25 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import org.whsv26.pnmemory.ui.notification.FcmTokenViewModel
 import androidx.activity.viewModels
+import org.whsv26.pnmemory.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
+  private lateinit var binding: ActivityMainBinding
   private val fcmTokenViewModel by viewModels<FcmTokenViewModel>()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-    // TODO Drop hardcoded token
-    val jwtTokenHardcoded = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3aHN2MjZAZ21haWwuY29tIiwiaXNzIjoid2hzdjI2IiwiaWF0IjoxNjI4MjY1NjY5LCJleHAiOjE2Mjg4NzA0Njl9.sc2_-7iGNSQIWdwH1-D9xiKDZFOpmzeEsUoL4o2BN5pjIclIGjujUwX-uxKihksaIUoorg3uVyTWjSuyw1xaCQ"
     val tokenPreferences = getSharedPreferences("TOKEN", Context.MODE_PRIVATE)
-    tokenPreferences.edit().putString("jwt_token", jwtTokenHardcoded).apply()
-
     val jwtToken = tokenPreferences.getString("jwt_token", "")!!;
 
-    val refreshRcmTokenButton: Button = findViewById(R.id.button_refresh_rcm_token)
-    refreshRcmTokenButton.setOnClickListener {
+    binding.buttonRefreshRcmToken.setOnClickListener {
       FirebaseInstallations.getInstance().getToken(false).addOnSuccessListener { res ->
         Toast.makeText(this, res.token, Toast.LENGTH_LONG).show()
-
         fcmTokenViewModel // trigger creation on ui thread
-
         CoroutineScope(IO).launch {
           fcmTokenViewModel.refresh(jwtToken, res.token)
         }
